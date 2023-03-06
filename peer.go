ed640e9d3d00534aff0c3f15b2f50a62fe4d1558
Igor Melo <igoracmelo@protonmail.com>
2023-03-06 04:27:18 -0300

    feat: peer address

--- peer.go
+++ peer.go
@@ -1,8 +1,15 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 type Peer struct {
 	Ip   net.IP
 	Port uint16
+}
+
+func (p *Peer) Address() string {
+	return net.JoinHostPort(string(p.Ip), fmt.Sprint(p.Port))
 }
--- peer_test.go
+++ peer_test.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"net"
+	"testing"
+)
+
+func Test_Peer_Address(t *testing.T) {
+	p := Peer{
+		Ip:   net.IP("123.456.789.0"),
+		Port: 54321,
+	}
+
+	want := "123.456.789.0:54321"
+	got := p.Address()
+	if got != want {
+		t.Errorf("want: %s, got: %s", want, got)
+	}
+}
