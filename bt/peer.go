f1d61c9c090463409133c59053a11844d025e065
Igor Melo <igoracmelo@protonmail.com>
2023-03-06 22:50:43 -0300

    feat: PeerConn

--- bt/peer.go
+++ bt/peer.go
@@ -13,3 +13,54 @@ type Peer struct {
 func (p *Peer) Address() string {
 	return net.JoinHostPort(string(p.Ip), fmt.Sprint(p.Port))
 }
+
+type PeerConn struct {
+	buf  []byte
+	bf   Bitfield
+	peer Peer
+	conn net.Conn
+	busy atomic.Bool
+}
+
+func NewPeerConn(peer Peer, myId [20]byte, infoHash [20]byte) (*PeerConn, error) {
+	conn, err := net.DialTimeout("tcp", peer.Address(), 3*time.Second)
+	if err != nil {
+		return nil, err
+	}
+
+	defer conn.SetDeadline(time.Time{})
+
+	myHs := &Handshake{
+		InfoHash: infoHash,
+		PeerId:   myId,
+	}
+
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	_, err = conn.Write(myHs.Bytes())
+	if err != nil {
+		return nil, err
+	}
+
+	hsBytes := make([]byte, 68)
+	_, err = conn.Read(hsBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	yourHs, err := HandshakeFrom(hsBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	if yourHs.InfoHash != myHs.InfoHash {
+		return nil, fmt.Errorf("infoHash do not match. want: %x, got: %x", myHs.InfoHash, yourHs.InfoHash)
+	}
+
+	return &PeerConn{
+		buf:  make([]byte, 4096),
+		peer: peer,
+		conn: conn,
+		busy: atomic.Bool{},
+	}, nil
+}
--- bt/peer_test.go
+++ bt/peer_test.go
@@ -1,6 +1,7 @@
 package bt
 
 import (
+	"log"
 	"net"
 	"testing"
 )
@@ -16,4 +17,41 @@ func Test_Peer_Address(t *testing.T) {
 	if got != want {
 		t.Errorf("want: %s, got: %s", want, got)
 	}
+}
+
+func Test_PeerConn_New(t *testing.T) {
+	infoHash := [20]byte([]byte("It's a me, InfoHash!"))
+
+	yourHs := Handshake{
+		InfoHash: infoHash,
+		PeerId:   [20]byte([]byte("It's a me, Rogi! Ok?")),
+	}
+
+	ready := make(chan struct{})
+
+	// dummy servers that only accepts a connection and sends the expected infohash
+	go func() {
+		l, err := net.Listen("tcp", "localhost:54321")
+		if err != nil {
+			panic(err)
+		}
+		close(ready)
+
+		urPeer, err := l.Accept()
+		if err != nil {
+			panic(err)
+		}
+		urPeer.Write(yourHs.Bytes())
+	}()
+
+	<-ready
+
+	peer := Peer{net.IP("localhost"), 54321}
+	myId := [20]byte([]byte("It's a me, Igor! Ok?"))
+
+	pc, err := NewPeerConn(peer, myId, infoHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pc.Close()
 }
